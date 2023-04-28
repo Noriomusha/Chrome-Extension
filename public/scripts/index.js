c7dba149e7575aa0a08448177bc52a0c088aeccd
Noriomusha <edward.i.alvarado@gmail.com>
2023-04-27 23:43:43 -0400

    Update index.js

---
+++ public/scripts/index.js
@@ -0,0 +1,7 @@
+let inputBtn = document.getElementById("inputBtn");
+const myLeads = [];
+const inputEl = document.getElementById("input-el");
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push("www.youtube.com");
+});

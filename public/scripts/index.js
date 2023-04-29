32c183b594f2d7fc6f784a4b1462e82108513b03
Noriomusha <edward.i.alvarado@gmail.com>
2023-04-28 23:23:07 -0400

    Update index.js
    update logic

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,30 @@
 let inputBtn = document.getElementById("inputBtn");
 const myLeads = [];
 const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
 
 inputBtn.addEventListener("click", function(){
-    myLeads.push("www.youtube.com");
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads()
 });
+
+function renderLeads(){
+    let listItems = "";
+    // creates the list items for the array
+// both create the list item and sets the list to a clickable hyperlink
+for(let i = 0; i<myLeads.length;i++){
+    
+    listItems += `
+        <li>
+            <a target = '_blank' href = "${myLeads[i]}> 
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
+}
+
+ulEl.innerHTML += listItems;
+}
+

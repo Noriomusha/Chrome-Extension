94b07c6c58e635e0484775e7bc47ce2d60f7946b
Noriomusha <edward.i.alvarado@gmail.com>
2023-04-29 22:13:41 -0400

    Update index.js
    fix logic

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,34 +1,46 @@
-let inputBtn = document.getElementById("inputBtn");
-let retrieveBtn = document.getElementById("retrieveBtn");
-const myLeads = [];
+const inputBtn = document.getElementById("inputBtn");
+
+const deleteAllBtn = document.getElementById("deleteAllBtn");
+const saveTabBtn = document.getElementById("saveTabBtn");
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
-
-localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
     inputEl.value = "";
-    renderLeads()
-});
-
-retrieveBtn.addEventListener("click", function(){
-    displayBookmarks();
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads)
 });
 
 
 
-function renderLeads(){
+deleteAllBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+    ulEl.innerHTML = "";
+})
+
+saveTabBtn.addEventListener("click", function(){
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            myLeads.push(tabs[0].url)
+            localStorage.setItem("myLeads", JSON.stringify(myLeads));
+            render(myLeads)
+        })   
+})
+
+function render(leads){
     let listItems = "";
-    // creates the list items for the array
+// creates the list items for the array
 // both create the list item and sets the list to a clickable hyperlink
-for(let i = 0; i<myLeads.length;i++){
+for(let i = 0; i<leads.length;i++){
     
     listItems += `
         <li>
-            <a target = '_blank' href = "${myLeads[i]}> 
-                ${myLeads[i]}
+            <a target = '_blank' href = "${leads[i]}> 
+                ${leads[i]}
             </a>
         </li>
         `
@@ -37,13 +49,4 @@ for(let i = 0; i<myLeads.length;i++){
 ulEl.innerHTML += listItems;
 }
 
-function displayBookmarks(){
-    bookmarks = JSON.parse(localStorage.getItem("myLeads"));
-    if(bookmarks){
-        myLeads = bookmarks
-        renderLeads()
-    }else{
-        alert("No bookmarks to display")
-    }
-    
-}
+
